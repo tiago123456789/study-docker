a62e4d3ae7fe141106bdebfb971f2f74195c1305
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2020-05-06 20:09:48 -0300

    Resolve problem installation composer

--- project-laravel/infrastructure/php-fpm/Entrypoint.sh
+++ project-laravel/infrastructure/php-fpm/Entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-php composer.phar install
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" \
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" \
+php composer-setup.php \
+php -r "unlink('composer-setup.php');" \
+chmod a+x composer.phar
+
+php -dmemory_limit=-1 composer.phar install
 php artisan key:generate
 php artisan migrate
 php-fpm
